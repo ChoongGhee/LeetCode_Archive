289db50151a3843253f5a57e4349e2498b447ac1
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-04-10 15:30:06 +0900

    Time: 0 ms (100%), Space: 7.6 MB (99.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,16 +3,15 @@ public:
     int climbStairs(int n) {
         if(n == 1) return 1;
 
-        int* dp = new int[2];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(char i =2;i<n+1;i++){
-            int temp = dp[0] + dp[1];
-            dp[0] = dp[1];
-            dp[1] = temp;
+        int prev = 1;
+        int curr = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            int temp = prev + curr;
+            prev = curr;
+            curr = temp;
         }
-
-        return dp[1];
+        
+        return curr;
     }
 };
