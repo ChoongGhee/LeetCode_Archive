ac9844778802d563408ac9e7078e46bb3a522121
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-04-16 16:05:20 +0900

    Time: 7 ms (88.04%), Space: 14.1 MB (28.43%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int result = abs(target+10001);
-        if(nums.size() == 3) return nums[0] + nums[1] + nums[2];
+        int result = nums[0] + nums[1] + nums[2];
+
+        if(nums.size() == 3) return result;
+        
         sort(nums.begin(), nums.end());
         
         for(int i=0;i<nums.size();i++){
             
-            // 실수2 : 중복되는 값이 있는지 확인 안함.
             if (i > 0 && nums[i] == nums[i-1]) continue;
             
             int left = i+1;
