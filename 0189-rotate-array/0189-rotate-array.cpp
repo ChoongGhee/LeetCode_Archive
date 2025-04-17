705311eefbd04058cedffc00db17ac5e1427abd4
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-04-17 22:01:46 +0900

    Time: 0 ms (100%), Space: 30.4 MB (16.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
         int n = nums.size();
-        k = k % n;
-        if(k==0) return;
-
+        k = k % n; 
+        if (k == 0) return; 
+        
         vector<int> temp = nums;
         
-        // 뒤쪽 k개의 요소를 앞으로 이동
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[n - k + i];
-        }
-        
-        // 앞쪽 (n-k)개의 요소를 뒤로 이동
-        for (int i = 0; i < n - k; i++) {
-            nums[k + i] = temp[i];
-        }    
+        for (int i = 0; i < k; i++) nums[i] = temp[n - k + i];
+        for (int i = 0; i < n - k; i++) nums[k + i] = temp[i];
     }
 };
