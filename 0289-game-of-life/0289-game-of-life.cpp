f1244904437a414bd9b24feeff35d6a81944de5d
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-04-18 15:46:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,48 +1,50 @@
 class Solution {
-public:
-    void gameOfLife(vector<vector<int>>& board) {
-        int row = board.size();
-        int col = board[0].size();
-
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                int cnt = 0;
-                
-                // 직접 8방향 이웃 확인 (방향 배열 없이)
-                for(int dx = -1; dx <= 1; dx++) {
-                    for(int dy = -1; dy <= 1; dy++) {
-                        if(dx == 0 && dy == 0) continue; // 자기 자신 제외
-                        
-                        int ni = i + dx;
-                        int nj = j + dy;
-                        
-                        if(ni >= 0 && ni < row && nj >= 0 && nj < col && 
-                           (board[ni][nj] == 1 || board[ni][nj] == 3)) {
-                            cnt++;
+    public:
+        // 이것도 최적화 가능
+    //     const vector<vector<int>> directions = {
+    //     {-1, -1}, {-1, 0}, {-1, 1},
+    //     {0, -1},          {0, 1},
+    //     {1, -1},  {1, 0}, {1, 1}
+    // };
+        void gameOfLife(vector<vector<int>>& board) {
+            // vector<vector<int>> temp = board;
+            int row = board.size();
+            int col = board[0].size();
+    
+            for(int i =0;i<row;i++){
+                for(int j = 0;j<col;j++){
+                    int cnt = 0;
+                    
+                    for(int dx = -1;dx <= 1; dx++){
+                        for(int dy = -1;dy<=1;dy++){
+                            if(-1< i+dx && i+dx < row && -1 < j+dy && j+dy < col && (board[i+dx][j+dy] == 1 || board[i+dx][j+dy] == 3)) cnt++;
                         }
-                    }
+                    }   
+                    
+                    if(board[i][j]) {if(cnt < 2 || cnt >3) board[i][j] = 3;}
+                    else if(cnt == 3) board[i][j] = 2;
+                    // 공간복잡도 최적화 : 상태 코드 4가지를 사용
+                    // 0, 1 그대로
+                    // 2 죽었는데 살아남. 이전엔 죽었다는 값
+                    // 3 살았는데 죽음. 이전엔 살아있음.
+                    // if((temp[i][j] && (cnt == 2 || cnt == 3)) || (!temp[i][j] && cnt == 3)) board[i][j] = 1;
+                    // else board[i][j] = 0;
+    
+                    // 실수 : 두 조건문 을 사용해 덮어씌워지는 현상이 일어나서 잘못됨
+                    // if(temp[i][j] && (cnt == 2 || cnt == 3)) board[i][j] = 1;
+                    // else board[i][j] = 0;
+                    
+                    // if(!temp[i][j] && cnt == 3) board[i][j] = 1;
+                    // else board[i][j] = 0;
                 }
-                
-                if(board[i][j] == 1) {
-                    // 살아있는 셀
-                    if(cnt < 2 || cnt > 3) {
-                        board[i][j] = 3; // 살아있는 상태 -> 죽은 상태
-                    }
-                } else {
-                    // 죽은 셀
-                    if(cnt == 3) {
-                        board[i][j] = 2; // 죽은 상태 -> 살아있는 상태
-                    }
+            }
+    
+            for(int i = 0; i < row; i++) {
+                for(int j = 0; j < col; j++) {
+                    if(board[i][j] == 2) board[i][j] = 1;
+                    else if(board[i][j] == 3) board[i][j] = 0;
                 }
             }
         }
+    };
 
-        // 최종 상태로 변환
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(board[i][j] == 2) board[i][j] = 1;
-                else if(board[i][j] == 3) board[i][j] = 0;
-            }
-        }
-    }
-};
