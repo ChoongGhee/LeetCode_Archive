da746e966bc3f711d22e649a7128e4174f0c691b
최재원 <133324658+ChoongGhee@users.noreply.github.com>
2025-04-20 00:04:31 +0900

    Time: 0 ms (100%), Space: 8.7 MB (10.13%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sidx = 0;
+        int tidx = 0;
+
+        while(sidx<s.size() && tidx<t.size()){
+            if(s[sidx] == t[tidx]){
+                sidx++;
+                tidx++;
+            }
+            else tidx++;
+        }
+
+        if(sidx == s.size()) return true;
+        else return false;
+
+    }
+};
